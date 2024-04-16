a86755df450bad4445828cecefb57daabb1debfc
PROGETOTEMPO <kauan220656@gmail.com>
2024-04-16 11:53:19 -0300

    dia1

---
+++ PROGETOTEMPO/Results.cs
@@ -0,0 +1,30 @@
+namespace PROGETOTEMPO;
+public class  results
+{
+public  string city {get; set;}
+
+public  int temp {get; set;}
+
+public  int humidity {get; set;}
+
+public  string Img_id{get; set;}
+
+public  string sunrise {get; set;}
+
+public  string sunset {get; set;}
+
+public  string timezone {get; set;}
+
+public  string moon_phase {get; set;}
+
+public  string currently {get; set;}
+
+public  string Wind_speedy {get; set;}
+
+public  string claudiness {get; set;}
+
+public  string codition_code {get; set;}
+
+public  string description {get; set;}
+
+}
