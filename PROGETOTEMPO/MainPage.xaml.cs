45faf1e6b82eaee52c49cff6decb770fc99f7a03
PROGETOTEMPO <kauan220656@gmail.com>
2024-04-19 11:05:50 -0300

    00004

--- PROGETOTEMPO/MainPage.xaml.cs
+++ PROGETOTEMPO/MainPage.xaml.cs
@@ -23,12 +23,39 @@ public partial class MainPage : ContentPage
 		resultado.description = "Tempo nublado";
 
 
+
+
 	}
 
 	void PreencherPagina()
 	{
-		labelTemp.Text = resultado.temp.ToString();
-		
+	 				labelTemp.Text = resultado.temp.ToString();
+
+	 				labeldescripition.Text = resultado.description();
+
+	 				labelcity.Text = resultado.city.ToString();
+
+	 				labelsunrise.Text = resultado.sunrise.ToString();
+
+	 				label.sunsetText = resultado.sunset.ToString();
+
+	 				labeltimizone.Text = resultado.timezone.ToString();
+
+	 				labelmoon_phase.Text = resultado.moon_phase.ToString();
+
+	 				labelcurrently.Text = resultado.currently.ToString();
+
+	 				labelwind_speedy.Text = resultado.Wind_speedy.ToString();
+
+	 				labelclaudiness.Text = resultado.claudiness.ToString();
+
+	 				labelcodition_code.Text = resultado.codition_code.ToString();
+
+	 				labelImg_id.Text = resultado.Img_id.ToString();
+
+	 				labelphumidity.Text = resultado.humidity.ToString();
+
+
 	}
 }
 
